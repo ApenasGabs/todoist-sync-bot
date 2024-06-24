21755d6006fcb170e7c0afe2e18b4f36e1a73b1f
Gabriel Rodrigues <gabers357@gmail.com>
2024-06-24 09:34:41 -0300

    chore: Update index.js and package.json

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const express = require('express');
 require('dotenv').config();
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => {
+app.use('/', (req, res, next) => {
+  console.log('next: ', next);
+  console.log(req.originalUrl);
+  console.log(req.body);
   res.send('Hello World!');
 });
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
+
