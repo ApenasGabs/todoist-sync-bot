bc2c2502c5f7fb4e0b957c1b7e1c2d9af1fc81e8
Gabriel Rodrigues <gabers357@gmail.com>
2024-06-25 20:59:56 -0300

    chore: Update package.json with body-parser dependency and add webhook route to index.js

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ app.use('/', (req, res, next) => {
   res.send('Hello World!');
 });
 
+app.post('/webhook', (req, res) => {
+  const payload = req.body;
+  console.log('Recebido webhook:', payload);
+
+  res.status(200).send('Webhook recebido com sucesso');
+});
+
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
